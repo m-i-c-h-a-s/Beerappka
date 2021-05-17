f18a9707d5b189ab60f96aa69d62889affcf8c35
Emil Pausz <emilpausz@gmail.com>
2021-05-17 19:45:19 +0200

    dodanie landing page, strony logowania, rejestracji

--- beerappkaFrontend/src/app/app.module.ts
+++ beerappkaFrontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { PageTopBarComponent } from './page-top-bar/page-top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    PageTopBarComponent
   ],
   imports: [
     BrowserModule,

ac16793c2efaa77b9a82d3f7ab2a21da4bd7bccf
Michal <michal.pojeta@gmail.com>
2021-05-18 16:05:35 +0200

    Skopiowanie brancha main do brancha frontend

--- beerappkaFrontend/src/app/app-routing.module.ts
+++ beerappkaFrontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'logowanie', component: LoginPageComponent },
+  { path: 'rejestracja', component: RegisterPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

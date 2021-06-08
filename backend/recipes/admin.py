4b0efd47f19dd3d0bf367957151605e410155353
Emil Pausz <emilpausz@gmail.com>
2021-06-08 16:19:29 +0200

    Dodanie modeli składników do receptury

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
 
-from recipes.models import Style, Recipe
+from recipes.models import (
+    Style, Recipe, Malt, Hops, Yeast,
+    Water, Manufacturer, RecipeHops,
+    RecipeMalt, RecipeYeast, RecipeWater
+)
+
+
+class RecipeHopsAdmin(admin.TabularInline):
+    model = RecipeHops
+    extra = 1
+
+
+class RecipeMaltAdmin(admin.TabularInline):
+    model = RecipeMalt
+    extra = 1
+
+
+class RecipeYeastAdmin(admin.TabularInline):
+    model = RecipeYeast
+    extra = 1
+
+
+class RecipeWaterAdmin(admin.TabularInline):
+    model = RecipeWater
+    extra = 1
 
 
 class StyleAdmin(admin.ModelAdmin):
@@ -8,8 +32,16 @@ class StyleAdmin(admin.ModelAdmin):
 
 
 class RecipeAdmin(admin.ModelAdmin):
-    pass
+    inlines = [
+        RecipeHopsAdmin, RecipeMaltAdmin,
+        RecipeYeastAdmin, RecipeWaterAdmin
+    ]
 
 
 admin.site.register(Style, StyleAdmin)
 admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Malt)
+admin.site.register(Hops)
+admin.site.register(Yeast)
+admin.site.register(Water)
+admin.site.register(Manufacturer)

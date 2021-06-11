9d2883782e86b8e5eb65f053cf62c88d1920de78
Emil Pausz <emilpausz@gmail.com>
2021-06-11 14:12:22 +0200

    dodanie API dla userów

---
+++ backend/profiles/api/urls.py
@@ -0,0 +1,8 @@
+from rest_framework import routers
+
+from profiles.api.views import UsersViewSet
+
+router = routers.DefaultRouter()
+router.register(r'users', UsersViewSet)
+
+urlpatterns = router.urls

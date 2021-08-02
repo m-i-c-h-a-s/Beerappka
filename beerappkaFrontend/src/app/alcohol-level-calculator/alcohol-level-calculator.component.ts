3589828a2f3055699b9797946ee4d359a5044c37
Michal <michal.pojeta@gmail.com>
2021-08-02 19:34:27 +0200

    Kalkulator ABV, kosmetyczne zmiany

--- beerappkaFrontend/src/app/alcohol-level-calculator/alcohol-level-calculator.component.ts
+++ beerappkaFrontend/src/app/alcohol-level-calculator/alcohol-level-calculator.component.ts
@@ -1,3 +1,4 @@
+  import { NumberFormatStyle } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlcoholLevelCalculatorComponent implements OnInit {
 
-  constructor() { }
+  public originalGravity: number | undefined;
+  public finalGravity: number | undefined;
+  public alcoholByVolume: number | string | undefined;
+  public apparentAttenuation: number | string | undefined;
+  public abvValue: number | undefined;
+  public attenuationValue: number | undefined;
+
+  calculateABV() {
+    if ( this.originalGravity != undefined && this.finalGravity != undefined
+      && this.originalGravity != null && this.finalGravity != null
+      && this.originalGravity != 0 && this.finalGravity != 0 ) {
+        this.abvValue = (this.originalGravity - this.finalGravity) / 1.938;
+        this.alcoholByVolume = this.abvValue.toFixed(2) + " %";
+    } else this.alcoholByVolume = '---';
+
+  }
+
+  calculateAttenuation() {
+    if ( this.originalGravity != undefined && this.finalGravity != undefined
+      && this.originalGravity != null && this.finalGravity != null
+      && this.originalGravity != 0 && this.finalGravity != 0 ) {
+        this.attenuationValue = ((this.originalGravity - this.finalGravity) / this.originalGravity) * 100;
+        this.apparentAttenuation = this.attenuationValue.toFixed(2) + " %";
+    } else this.apparentAttenuation = '---';
+  }
+
+  constructor() {
+    this.alcoholByVolume = '---';
+    this.apparentAttenuation = '---';
+  }
 
   ngOnInit(): void {
   }

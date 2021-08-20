c818c3cb082d6116ac5279d566f97eea050787e2
Emil Pausz <emilpausz@gmail.com>
2021-08-20 13:54:50 +0200

    widok api Warki - dodanie info o userze i recipe

--- backend/batches/api/serializers.py
+++ backend/batches/api/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
 
 from batches.models import Batch
+from profiles.api.serializers import UserSerializer
+from recipes.api.serializers import RecipeSerializer
 
 
 class BatchSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    recipe = RecipeSerializer()
 
     class Meta:
         model = Batch

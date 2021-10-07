31cb7c740dbfce1b35134d23583a49cde9a4c64e
legendemil <emilpausz@gmail.com>
2021-10-07 19:55:16 +0200

    poprawka

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-*@!lz@0bf$0nasm2@4heg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", 'localhost localhost:4200').split(' ')
 
 
 # Application definition

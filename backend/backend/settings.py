93c350c446cf2f47d122ae71e1788c049703eb55
legendemil <emilpausz@gmail.com>
2021-10-17 15:35:47 +0200

    poprawka whitenoise

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -161,7 +161,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")

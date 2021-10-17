dcb2934f362ee8caab1e16776c5d8f8aa2790d51
legendemil <emilpausz@gmail.com>
2021-10-17 18:56:29 +0200

    dodanie wysylki maili

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -194,6 +194,14 @@ OLD_PASSWORD_FIELD_ENABLED = True
 
 if DEBUG:
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = os.environ.get("EMAIL_HOST", "")
+    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
+    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
+    EMAIL_PORT = os.environ.get("EMAIL_PORT", "")
+    EMAIL_USE_TLS = True
+    DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", "")
 
 CORS_ALLOWED_ORIGINS = os.environ.get(
     'CORS_ALLOWED_ORIGINS', 'http://localhost:4200'

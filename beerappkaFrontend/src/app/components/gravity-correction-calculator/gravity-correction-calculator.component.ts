ceb18c5b5c0cab76b914e333b5a6e3b36131efeb
Michal <michal.pojeta@gmail.com>
2021-10-21 18:58:57 +0200

    Kalkulator - korekta gęstości (wyliczanie)

--- beerappkaFrontend/src/app/components/gravity-correction-calculator/gravity-correction-calculator.component.ts
+++ beerappkaFrontend/src/app/components/gravity-correction-calculator/gravity-correction-calculator.component.ts
@@ -10,6 +10,7 @@ export class GravityCorrectionCalculatorComponent implements OnInit {
   public amountOfBeerInLiters: number | null;
   public currentGravity: number | null;
   public expectedGravity: number | null;
+  public calculationResult: string = '';
 
 
   constructor() {
@@ -23,6 +24,28 @@ export class GravityCorrectionCalculatorComponent implements OnInit {
 
 
   calculateCorrection() {
+    var waterAdditionValue: number | undefined;
+    let sugarAdditionValue: number | undefined;
+    var waterAddition: string | undefined;
+    let sugarAddition: string | undefined;
 
+    if (this.amountOfBeerInLiters != null && this.currentGravity != null && this.expectedGravity != null
+    && this.amountOfBeerInLiters > 0 && this.currentGravity > 0 && this.expectedGravity > 0) {
+      if (this.currentGravity > this.expectedGravity) {
+        waterAdditionValue = ((this.currentGravity - this.expectedGravity) / this.expectedGravity) * this.amountOfBeerInLiters;
+        waterAddition = waterAdditionValue.toFixed(1);
+        this.calculationResult = "W celu zmniejszenia poziomu gęstości dodaj " + waterAddition + "L wody.";
+      }
+      else if (this.currentGravity < this.expectedGravity) {
+        sugarAdditionValue = (this.expectedGravity - this.currentGravity) * this.amountOfBeerInLiters * 11;
+        sugarAddition = sugarAdditionValue.toFixed(0);
+        this.calculationResult = "W celu zwiększenia poziomu gęstości dodaj " + sugarAddition + "g cukru.";
+      }
+      else {
+        this.calculationResult = "";
+      }
+    } else {
+      this.calculationResult = "";
+    }
   }
 }

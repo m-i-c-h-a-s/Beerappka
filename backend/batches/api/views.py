92fea95038aeb5dd044a7ca1cf1862aad80fedbf
legendemil <emilpausz@gmail.com>
2021-11-16 17:59:26 +0100

    dodanie widoku niezabutelkowane warki

--- backend/batches/api/views.py
+++ backend/batches/api/views.py
@@ -1,4 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from batches.api.filters import BatchFilter
 from batches.api.serializers import BatchSerializer, BatchCreateUpdateSerializer
@@ -14,6 +16,7 @@ class BatchesViewSet(ModelViewSet):
         'create': BatchCreateUpdateSerializer,
         'update': BatchCreateUpdateSerializer,
         'partial_update': BatchCreateUpdateSerializer,
+        'not_bottled': BatchSerializer
     }
     filterset_class = BatchFilter
 
@@ -26,3 +29,17 @@ class BatchesViewSet(ModelViewSet):
     def perform_create(self, serializer):
         user = self.request.user
         serializer.save(user=user)
+
+    @action(detail=False)
+    def not_bottled(self, request):
+        queryset = self.get_queryset().filter(bottling_date__isnull=True)
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True, context={
+                'request': request
+            })
+            return self.get_paginated_response(serializer.data)
+        serializer = self.get_serializer(queryset, many=True, context={
+            'request': request
+        })
+        return Response(serializer.data) 
